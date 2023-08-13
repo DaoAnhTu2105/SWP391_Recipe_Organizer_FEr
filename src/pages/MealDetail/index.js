e89537be68d31b4ff1c109dd5dcf89fa4a4e6817
Vinh Nguyễn <vinhnguyen50501@gmail.com>
2023-08-13 16:22:37 +0700

    add nutrition to meal plan detail

--- src/pages/MealDetail/index.js
+++ src/pages/MealDetail/index.js
@@ -37,15 +37,36 @@ const MealDetail = () => {
                             </button>
                         </div>
                     </div>
-                    <Food
-                        image="https://i.ytimg.com/vi/A_o2qfaTgKs/maxresdefault.jpg"
-                        name="Bun bo"
-                        time="20"
-                        ingredient="10"
-                    />
+                    <div className='title'>
+                        <h4>
+                            Nutrition Facts (per serving)
+                        </h4>
+                        <div className='nutrition'>
+                            <div className='nutrition-component'>
+                                Calories: <b>409</b>
+                            </div>
+                            <div className='nutrition-component'>
+                                Fat: <b>8g</b>
+                            </div>
+                            <div className='nutrition-component'>
+                                Carbs: <b>73g</b>
+                            </div>
+                            <div className='nutrition-component'>
+                                Protein: <b>11g</b>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='title'>
+                        <Food
+                            image="https://i.ytimg.com/vi/A_o2qfaTgKs/maxresdefault.jpg"
+                            name="Bun bo"
+                            time="20"
+                            ingredient="10"
+                        />
+                    </div>
+
                 </div>
                 <div className='ingredient'>
-
                 </div>
             </div>
         </Fragment>
