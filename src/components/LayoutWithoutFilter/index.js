938c2b95a8dc74a4418b60deb32076249d25e56a
Vinh Nguyễn <vinhnguyen50501@gmail.com>
2023-08-14 20:44:29 +0700

    change footer

--- src/components/LayoutWithoutFilter/index.js
+++ src/components/LayoutWithoutFilter/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from '../Header/Header'
-import Footer from '../Footer'
-import Search from '../Search/Search'
+import Footer from '../Footer/Footer'
 
 const DefaultLayout = ({ children }) => {
     return (
