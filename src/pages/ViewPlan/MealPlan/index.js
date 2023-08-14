5d734db957c0c1e43be461916686991d4b6b8a37
Vinh Nguyễn <vinhnguyen50501@gmail.com>
2023-08-14 22:18:20 +0700

    mock data table plan

--- src/pages/ViewPlan/MealPlan/index.js
+++ src/pages/ViewPlan/MealPlan/index.js
@@ -100,119 +100,6 @@ const meal = {
     ],
 }
 
-// export default function MealPlan() {
-//     return (
-//         <div className="plan-meal">
-//             <div className="table-header">
-//                 <div></div>
-//                 <div className="">
-//                     <a href="/meal-detail">Monday</a>
-//                 </div>
-//                 <div className="">Tuesday</div>
-//                 <div className="">Wednesday</div>
-//                 <div className="">Thursday</div>
-//                 <div className="">Friday</div>
-//                 <div className="">Saturday</div>
-//                 <div className="">Sunday</div>
-//             </div>
-//             <div className="table-body">
-//                 <div className="table-body-content">
-//                     <div className="meal" style={{ color: '#32a6de' }}>
-//                         <div>
-//                             <b>BreakFast</b>
-//                         </div>
-//                         <div>
-//                             <b>6AM - 8AM</b>
-//                         </div>
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Perfect Pancakes'} calo={'409'} meal={'breakfast'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Slow Cooker Buffalo Chicken Sandwiches'} calo={'578'} meal={'breakfast'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Fiesta Slow Cooker Shredded Chicken Tacos'} calo={'71'} meal={'breakfast'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Deep-Fried Oreos'} calo={'156'} meal={'breakfast'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Funnel Cakes'} calo={'524'} meal={'breakfast'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Fried Egg Tortilla'} calo={'473'} meal={'breakfast'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Breakfast Burrito Bowl'} calo={'152'} meal={'breakfast'} />
-//                     </div>
-//                 </div>
-//                 <div className="table-body-content">
-//                     <div style={{ color: '#e29d1d' }}>
-//                         <div>
-//                             <b>Lunch</b>
-//                         </div>
-//                         <div>
-//                             <b>12:30AM - 2PM</b>
-//                         </div>
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Deep-Fried Oreos'} calo={'450'} meal={'lunch'} />
-//                         <Food foodName={'Sesame Noodles'} calo={'450'} meal={'lunch'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Day-After-Thanksgiving Turkey Carcass Soup'} calo={'370'} meal={'lunch'} />
-//                         <Food foodName={'Quick Italian Pasta Salad'} calo={'371'} meal={'lunch'} />
-//                     </div>
-//                     <div className="item"> <Food foodName={'Lemon Chicken Orzo Soup'} calo={'187'} meal={'lunch'} /></div>
-//                     <div className="item">
-//                         <Food foodName={'Quick Italian Pasta Salad'} calo={'63'} meal={'lunch'} />
-//                         <Food foodName={'Garlic Bread Spread'} calo={'167'} meal={'lunch'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Awesome Hot Ham and Cheese'} calo={'200'} meal={'lunch'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Eggplant Caponata (Sicilian Version)'} calo={'320'} meal={'lunch'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Pastrami Reuben Sandwich'} calo={'380'} meal={'lunch'} />
-//                     </div>
-//                 </div>
-//                 <div className="table-body-content">
-//                     <div style={{ color: '#68169c' }}>
-//                         <div>
-//                             <b>Dinner</b>
-//                         </div>
-//                         <div>
-//                             <b>6PM - 9PM</b>
-//                         </div>
-//                     </div>
-//                     <div className="item"><Food foodName={'Grilled Turkey Legs'} calo={'450'} /></div>
-//                     <div className="item">
-//                         <Food foodName={'Tex-Mex Pork Chops and Rice Skillet'} calo={'682'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Lasagna Flatbread'} calo={'620'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Slow-Cooker Corned Beef and Cabbage'} calo={'670'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Stout-Braised Lamb Shanks'} calo={'620'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Empanadas (Beef Turnovers)'} calo={'620'} />
-//                     </div>
-//                     <div className="item">
-//                         <Food foodName={'Slow Cooker Texas Pulled Pork'} calo={'620'} />
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
 export default function MealPlan() {
     return (
         <div className="plan-meal">
@@ -238,22 +125,27 @@ export default function MealPlan() {
                             <b>6AM - 8AM</b>
                         </div>
                     </div>
-                    {meal.food.map((meal) => (
-                        <div className="item">
-                            {meal.breakfast.map((food) => {
-                                if (food.food.length !== 0) { 
-                                    return (
-                                        <Food
-                                            foodName={food.food}
-                                            calo={food.calo}
-                                            meal='breakfast'
-                                        />
-                                    )
-                                }
-                                return null;
-                            })}
-                        </div>
-                    ))}
+                    <div className="item">
+                        <Food foodName={'Perfect Pancakes'} calo={'409'} meal={'breakfast'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Slow Cooker Buffalo Chicken Sandwiches'} calo={'578'} meal={'breakfast'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Fiesta Slow Cooker Shredded Chicken Tacos'} calo={'71'} meal={'breakfast'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Deep-Fried Oreos'} calo={'156'} meal={'breakfast'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Funnel Cakes'} calo={'524'} meal={'breakfast'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Fried Egg Tortilla'} calo={'473'} meal={'breakfast'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Breakfast Burrito Bowl'} calo={'152'} meal={'breakfast'} />
+                    </div>
                 </div>
                 <div className="table-body-content">
                     <div style={{ color: '#e29d1d' }}>
@@ -264,22 +156,28 @@ export default function MealPlan() {
                             <b>12:30AM - 2PM</b>
                         </div>
                     </div>
-                    {meal.food.map((meal) => (
-                        <div className="item">
-                            {meal.lunch.map((food) => {
-                                if (food.food.length !== 0) {
-                                    return (
-                                        <Food
-                                            foodName={food.food}
-                                            calo={food.calo}
-                                            meal='lunch'
-                                        />
-                                    )
-                                }
-                                return null;
-                            })}
-                        </div>
-                    ))}
+                    <div className="item">
+                        <Food foodName={'Deep-Fried Oreos'} calo={'450'} meal={'lunch'} />
+                        <Food foodName={'Sesame Noodles'} calo={'450'} meal={'lunch'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Day-After-Thanksgiving Turkey Carcass Soup'} calo={'370'} meal={'lunch'} />
+                        <Food foodName={'Quick Italian Pasta Salad'} calo={'371'} meal={'lunch'} />
+                    </div>
+                    <div className="item"> <Food foodName={'Lemon Chicken Orzo Soup'} calo={'187'} meal={'lunch'} /></div>
+                    <div className="item">
+                        <Food foodName={'Quick Italian Pasta Salad'} calo={'63'} meal={'lunch'} />
+                        <Food foodName={'Garlic Bread Spread'} calo={'167'} meal={'lunch'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Awesome Hot Ham and Cheese'} calo={'200'} meal={'lunch'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Eggplant Caponata (Sicilian Version)'} calo={'320'} meal={'lunch'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Pastrami Reuben Sandwich'} calo={'380'} meal={'lunch'} />
+                    </div>
                 </div>
                 <div className="table-body-content">
                     <div style={{ color: '#68169c' }}>
@@ -290,23 +188,125 @@ export default function MealPlan() {
                             <b>6PM - 9PM</b>
                         </div>
                     </div>
-                    {meal.food.map((meal) => (
-                        <div className="item">
-                            {meal.dinner.map((food) => {
-                                if (food.food.length !== 0) {
-                                    return (
-                                        <Food
-                                            foodName={food.food}
-                                            calo={food.calo}
-                                        />
-                                    )
-                                }
-                                return null;
-                            })}
-                        </div>
-                    ))}
+                    <div className="item"><Food foodName={'Grilled Turkey Legs'} calo={'450'} /></div>
+                    <div className="item">
+                        <Food foodName={'Tex-Mex Pork Chops and Rice Skillet'} calo={'682'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Lasagna Flatbread'} calo={'620'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Slow-Cooker Corned Beef and Cabbage'} calo={'670'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Stout-Braised Lamb Shanks'} calo={'620'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Empanadas (Beef Turnovers)'} calo={'620'} />
+                    </div>
+                    <div className="item">
+                        <Food foodName={'Slow Cooker Texas Pulled Pork'} calo={'620'} />
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
+
+// export default function MealPlan() {
+//     return (
+//         <div className="plan-meal">
+//             <div className="table-header">
+//                 <div></div>
+//                 <div className="">
+//                     <a href="/meal-detail">Monday</a>
+//                 </div>
+//                 <div className="">Tuesday</div>
+//                 <div className="">Wednesday</div>
+//                 <div className="">Thursday</div>
+//                 <div className="">Friday</div>
+//                 <div className="">Saturday</div>
+//                 <div className="">Sunday</div>
+//             </div>
+//             <div className="table-body">
+//                 <div className="table-body-content">
+//                     <div className="meal" style={{ color: '#32a6de' }}>
+//                         <div>
+//                             <b>BreakFast</b>
+//                         </div>
+//                         <div>
+//                             <b>6AM - 8AM</b>
+//                         </div>
+//                     </div>
+//                     {meal.food.map((meal) => (
+//                         <div className="item">
+//                             {meal.breakfast.map((food) => {
+//                                 if (food.food.length !== 0) {
+//                                     return (
+//                                         <Food
+//                                             foodName={food.food}
+//                                             calo={food.calo}
+//                                             meal='breakfast'
+//                                         />
+//                                     )
+//                                 }
+//                                 return null;
+//                             })}
+//                         </div>
+//                     ))}
+//                 </div>
+//                 <div className="table-body-content">
+//                     <div style={{ color: '#e29d1d' }}>
+//                         <div>
+//                             <b>Lunch</b>
+//                         </div>
+//                         <div>
+//                             <b>12:30AM - 2PM</b>
+//                         </div>
+//                     </div>
+//                     {meal.food.map((meal) => (
+//                         <div className="item">
+//                             {meal.lunch.map((food) => {
+//                                 if (food.food.length !== 0) {
+//                                     return (
+//                                         <Food
+//                                             foodName={food.food}
+//                                             calo={food.calo}
+//                                             meal='lunch'
+//                                         />
+//                                     )
+//                                 }
+//                                 return null;
+//                             })}
+//                         </div>
+//                     ))}
+//                 </div>
+//                 <div className="table-body-content">
+//                     <div style={{ color: '#68169c' }}>
+//                         <div>
+//                             <b>Dinner</b>
+//                         </div>
+//                         <div>
+//                             <b>6PM - 9PM</b>
+//                         </div>
+//                     </div>
+//                     {meal.food.map((meal) => (
+//                         <div className="item">
+//                             {meal.dinner.map((food) => {
+//                                 if (food.food.length !== 0) {
+//                                     return (
+//                                         <Food
+//                                             foodName={food.food}
+//                                             calo={food.calo}
+//                                         />
+//                                     )
+//                                 }
+//                                 return null;
+//                             })}
+//                         </div>
+//                     ))}
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
