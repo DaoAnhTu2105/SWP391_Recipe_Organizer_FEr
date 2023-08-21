2c1811286f5247d29cb3b93e719e996508991b25
Vinh Nguyễn <vinhnguyen50501@gmail.com>
2023-08-21 08:40:16 +0700

    add confirm to update role user list

--- src/pages/UserList/index.js
+++ src/pages/UserList/index.js
@@ -208,16 +208,7 @@ export default function UserList() {
                 }
             });
         } else {
-            toast.success("Update Success", {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "colored",
-            });
+            toast.success('co cl')
         }
     }
 
