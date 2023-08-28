d9cf3bf49b9b5770198901893dcd072372cbe7c5
DaoAnhTu2105 <tudase151149@gmail.com>
2023-08-28 11:49:12 +0700

    fix layout recipedetail and add isDelete homepage recipeDetail and full search

--- src/pages/SearchResult/searchFavorite.js
+++ src/pages/SearchResult/searchFavorite.js
@@ -1,15 +1,63 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import { Card, CardMedia, Rating, CardContent, Typography, Box } from '@mui/material'
+import {
+    Card,
+    CardMedia,
+    Rating,
+    CardContent,
+    Typography,
+    Box,
+    Button,
+    CardActions,
+} from '@mui/material'
 import SearchFilter from '../FavortieRecipe/SearchFavoriteFilter'
+import ErrorIcon from '@mui/icons-material/Error'
+import toast, { Toaster } from 'react-hot-toast'
+import Swal from 'sweetalert2'
+
+import { useDispatch } from 'react-redux'
+import { removeFavor } from '../../redux/apiThunk/getFavoriteUserThunk'
+import { useNavigate } from 'react-router-dom'
 
 const SearchResultFavorite = () => {
     const location = useLocation()
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
     const searchResult = location.state?.searchResult
     const result = searchResult?.data
+
     console.log(result)
+    const handleConfirmRemove = async (id) => {
+        console.log('id', id)
+        Swal.fire({
+            title: 'Remove your favorite!',
+            text: 'Do you want to remove your favorite?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes',
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                await dispatch(removeFavor(id))
+                    .then(async (result) => {
+                        if (result.payload && result.payload.message === 'Success') {
+                            await toast.success('Remove successful!!!')
+                            navigate('/favorite-recipe')
+                        } else {
+                            toast.error('Remove failed!')
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+            }
+        })
+    }
+
     return (
         <>
+            <Toaster position="top-center" reverseOrder={false} />
             <SearchFilter />
             <div className="container">
                 <h2 style={{ fontWeight: 600, color: '#f39c12', textAlign: 'center' }}>
@@ -54,11 +102,79 @@ const SearchResultFavorite = () => {
                         >
                             {result &&
                                 Array.isArray(result) &&
-                                result.map((recipe) => (
-                                    <div className="grid-item" key={recipe.recipeId}>
-                                        <Card style={{ width: 345, maxHeight: 470 }}>
-                                            <Link to={`/recipe-detail/${recipe.recipeId}`}>
-                                                <Box style={{ textAlign: 'center' }}>
+                                result.map((recipe) =>
+                                    !recipe.isDelete ? (
+                                        <div className="grid-item" key={recipe.recipeId}>
+                                            <Card style={{ width: 345, maxHeight: 470 }}>
+                                                <Link to={`/recipe-detail/${recipe.recipeId}`}>
+                                                    <Box style={{ textAlign: 'center' }}>
+                                                        <CardMedia
+                                                            component="img"
+                                                            style={{ width: 350, height: 194 }}
+                                                            image={recipe.photoVMs[0].photoName}
+                                                            alt="Perfect Pancakes"
+                                                        />
+                                                        <Rating
+                                                            name="read-only"
+                                                            value={recipe.aveVote}
+                                                            readOnly
+                                                            size="small"
+                                                            precision={0.5}
+                                                            sx={{ mt: 2 }}
+                                                        />
+                                                    </Box>
+                                                </Link>
+                                                <CardContent style={{ textAlign: 'center' }}>
+                                                    <Typography
+                                                        variant="body1"
+                                                        color="text.primary"
+                                                        style={{
+                                                            fontWeight: 600,
+                                                            fontSize: 15,
+                                                        }}
+                                                    >
+                                                        {recipe.recipeName}
+                                                    </Typography>
+                                                    <Typography
+                                                        variant="body3"
+                                                        color="text.secondary"
+                                                    >
+                                                        {new Date(
+                                                            recipe.updateTime
+                                                        ).toLocaleDateString()}
+                                                    </Typography>
+                                                    <CardActions
+                                                        sx={{
+                                                            display: 'flex',
+                                                            justifyContent: 'center',
+                                                        }}
+                                                    >
+                                                        <Button
+                                                            size="small"
+                                                            style={{
+                                                                outline: 'none',
+                                                                color: '#f39c12',
+                                                            }}
+                                                            onClick={() =>
+                                                                handleConfirmRemove(recipe.recipeId)
+                                                            }
+                                                        >
+                                                            Remove
+                                                        </Button>
+                                                    </CardActions>
+                                                </CardContent>
+                                            </Card>
+                                        </div>
+                                    ) : (
+                                        <div className="grid-item" key={recipe.recipeId}>
+                                            <Card
+                                                style={{
+                                                    width: 345,
+                                                    maxHeight: 470,
+                                                    textAlign: 'center',
+                                                }}
+                                            >
+                                                <Link to={`/recipe-detail/${recipe.recipeId}`}>
                                                     <CardMedia
                                                         component="img"
                                                         style={{ width: 350, height: 194 }}
@@ -73,30 +189,48 @@ const SearchResultFavorite = () => {
                                                         precision={0.5}
                                                         sx={{ mt: 2 }}
                                                     />
-                                                </Box>
-
-                                                <CardContent style={{ textAlign: 'center' }}>
-                                                    <Typography
-                                                        variant="body1"
-                                                        color="text.primary"
-                                                        style={{ fontWeight: 600, fontSize: 15 }}
+                                                </Link>
+                                                <CardContent>
+                                                    <Box
+                                                        sx={{
+                                                            display: 'flex',
+                                                            alignItems: 'center',
+                                                            justifyContent: 'center',
+                                                        }}
                                                     >
-                                                        {recipe.recipeName}
-                                                    </Typography>
-                                                    <br></br>
-                                                    <Typography
-                                                        variant="body3"
-                                                        color="text.secondary"
+                                                        <Typography
+                                                            variant="body2"
+                                                            color="text.primary"
+                                                        >
+                                                            {recipe.recipeName}
+                                                        </Typography>
+                                                        &nbsp; &nbsp;
+                                                        <ErrorIcon style={{ color: 'red' }} />
+                                                    </Box>
+                                                    <CardActions
+                                                        sx={{
+                                                            display: 'flex',
+                                                            justifyContent: 'center',
+                                                        }}
                                                     >
-                                                        {new Date(
-                                                            recipe.updateTime
-                                                        ).toLocaleDateString()}
-                                                    </Typography>
+                                                        <Button
+                                                            size="small"
+                                                            style={{
+                                                                outline: 'none',
+                                                                color: '#f39c12',
+                                                            }}
+                                                            onClick={() =>
+                                                                handleConfirmRemove(recipe.recipeId)
+                                                            }
+                                                        >
+                                                            Remove
+                                                        </Button>
+                                                    </CardActions>
                                                 </CardContent>
-                                            </Link>
-                                        </Card>
-                                    </div>
-                                ))}
+                                            </Card>
+                                        </div>
+                                    )
+                                )}
                         </div>
                     </>
                 )}
